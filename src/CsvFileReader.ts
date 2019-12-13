51375df75c951c8bc711ce0b7a0fd95ecc3271a9
Siva Krishna <shivataraya@gmail.com>
2019-12-13 07:21:12 -0600

    refactor csvreader to abtract and generic class

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,10 +1,9 @@
 import fs from "fs";
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResult";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
+
+  abstract mapRow(row: string[]): T;
 
   constructor(public fileName: string) {}
 
@@ -15,18 +14,6 @@ export class CsvFileReader {
       })
       .split("\n")
       .map((row: string): string[] => row.split(","))
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6]
-          ];
-        }
-      );
+      .map((row: string[]): T => this.mapRow(row));
   }
 }
